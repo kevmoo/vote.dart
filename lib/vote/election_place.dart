af50a3c1725bd646a32ec70f2ea759118e6f1c5e
Kevin Moore <kevin@thinkpixellab.com>
2012-06-16 23:16:19 -0700

    ElectionPlace: use ReadOnlyCollection

--- lib/vote/election_place.dart
+++ lib/vote/election_place.dart
@@ -1,19 +1,13 @@
 class ElectionPlace<TCandidate extends Player>
-  extends ListBase<TCandidate> {
-  final List<TCandidate> _candidates;
+  extends ReadOnlyCollection<TCandidate> {
   final int place;
 
-  ElectionPlace._internal(this.place, this._candidates);
+  ElectionPlace._internal(this.place, Iterable<TCandidate> candidates) :
+    super(candidates);
 
   factory ElectionPlace(int place, Iterable<TCandidate> candidates) {
     assert(place > 0);
     assert(candidates != null);
     return new ElectionPlace._internal(place, new List<TCandidate>.from(candidates));
-  }
-
-  int get length() => _candidates.length;
-
-  TCandidate operator [](int index) {
-    return _candidates[index];
   }
 }
