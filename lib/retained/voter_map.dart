83bba27a9783a1a92433b63521f465e51d1cbfb5
Kevin Moore <kevin@thinkpixellab.com>
2012-07-03 17:35:23 -0700

    VoterMap: starting to dray actual data points

--- lib/retained/voter_map.dart
+++ lib/retained/voter_map.dart
@@ -1,10 +1,9 @@
 class VoterMap extends PElement {
-  static final num _count = 10;
   core.Coordinate _mouse;
-  final HashSet<MapPlayer> _points;
+  final HashSet<MapPlayer> _players;
 
   VoterMap(int w, int h) :
-    _points = new HashSet<MapPlayer>(),
+    _players = new HashSet<MapPlayer>(),
     super(w, h);
 
   core.Coordinate get mouse(){
@@ -15,24 +14,31 @@ class VoterMap extends PElement {
     _mouse = value;
   }
 
+  Iterable<MapPlayer> get players() => _players;
+
+  void set players(Iterable<MapPlayer> value) {
+    core.requireArgumentNotNull(value, "value");
+    _players.clear();
+    _players.addAll(value);
+    invalidateDraw();
+  }
+
   void drawOverride(CanvasRenderingContext2D ctx){
     ctx.fillStyle = 'red';
-    var w = width / _count;
-    var h = height / _count;
+    final scale = 30;
     var margin = 3;
 
-    for(var x = 0; x < _count; x++){
-      for(var y = 0; y < _count; y++){
-        CanvasUtil.ellipse(ctx, x * w + margin, y * h + margin, w - 2 * margin, h - 2 * margin);
-        ctx.fill();
-      }
+    for(final player in _players) {
+      final x = player.location.x;
+      final y = player.location.y;
+      CanvasUtil.ellipse(ctx, x * scale + margin, y * scale + margin, scale - 2 * margin, scale - 2 * margin);
+      ctx.fill();
     }
 
     ctx.fillStyle = 'blue';
     if(_mouse != null){
-      CanvasUtil.ellipse(ctx, _mouse.x - w/2, _mouse.y - h/2, w - 2 * margin, h - 2 * margin);
+      CanvasUtil.ellipse(ctx, _mouse.x - scale/2, _mouse.y - scale/2, scale - 2 * margin, scale - 2 * margin);
       ctx.fill();
     }
-
   }
 }
--- samples/vote/vote_demo.dart
+++ samples/vote/vote_demo.dart
@@ -1,6 +1,8 @@
 #import('dart:html');
 #import('../../../dartlib/lib/core.dart', prefix:'core');
 #import('../../../dartlib/lib/retained.dart');
+#import('../../lib/vote.dart');
+#import('../../lib/map.dart');
 #import('../../lib/retained.dart');
 
 main(){
@@ -16,6 +18,31 @@ class VoteDemo{
 
   factory VoteDemo(CanvasElement canvas){
     var voterMap = new VoterMap(canvas.width, canvas.height);
+
+    //
+    // Populate
+    //
+    // 11 voters from 0,0 to 10,0
+    var voters = new List<MapPlayer>();
+    for(var i = 0; i <= 10; i++) {
+      voters.add(new MapPlayer(new core.Coordinate(i,0)));
+    }
+
+    voterMap.players = voters;
+
+    // center at 5
+    //var canCenter = new MapPlayer(const core.Coordinate(5, 0));
+
+    // left at 4
+    //var canLeft = new MapPlayer(const core.Coordinate(4, 0));
+
+    // spoiler at 7
+    //var canSpoiler = new MapPlayer(const core.Coordinate(7, 0));
+
+
+    //
+    // Create the stage, etc
+    //
 
     var stage = new Stage(canvas, voterMap);
 
