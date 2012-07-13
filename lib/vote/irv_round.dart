b311201f9ecc2c66a9ee3b6110c1f9be05cc4099
Kevin Moore <kevin@thinkpixellab.com>
2012-07-13 11:26:24 -0700

    IrvElection: starting
    Fun with Issue #4

---
+++ lib/vote/irv_round.dart
@@ -0,0 +1,18 @@
+class IrvRound {
+
+}
+
+/*
+ * Factory
+ * - ballots
+ * - eliminated candidates
+ *
+ * Properties
+ * - hasWinner
+ * - ranked output (type?)
+ * - eliminated candidates
+ * - effective ballots? -> ballots after eliminated candidates
+ *
+ *
+ *
+ */
