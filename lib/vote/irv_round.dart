4f13038dce84c8de79d9a4990919ea64b90b122e
Kevin Moore <kevin@thinkpixellab.com>
2012-07-14 00:25:14 -0700

    IrvRound: slowly slowly

--- lib/vote/irv_round.dart
+++ lib/vote/irv_round.dart
@@ -21,16 +21,16 @@ class IrvRound<TVoter extends Player, TCandidate extends Player> {
     final placeVotes = $(voteGroups.getKeys()).toList();
     // reverse sorting -> most votes first
     placeVotes.sort((a,b) => b.compareTo(a));
-    print(placeVotes);
 
+    int placeNumber = 1;
     int totalVotes = 0;
-    candidateAllocations.forEach((k,v) {
-      assert(v != null);
-      assert(v.length > 0);
-      totalVotes += v.length;
-    });
-
-    print(totalVotes);
+    final places = $(placeVotes).select((pv) {
+      final vg = voteGroups[pv];
+      final currentPlaceNumber = placeNumber;
+      totalVotes += vg.length * pv;
+      placeNumber += vg.length;
+      return new PluralityElectionPlace<TCandidate>(currentPlaceNumber, vg, pv);
+    }).toList();
   }
 
 }
