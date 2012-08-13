fccc024b3d75e99fedf9c476bd4135176c549b92
Kevin Moore <kevin@thinkpixellab.com>
2012-08-12 23:00:18 -0400

    using HtmlView from dart lib

--- lib/html.dart
+++ lib/html.dart
@@ -3,10 +3,10 @@
 #import('dart:html');
 
 #import('../../dartlib/lib/dartlib.dart', prefix:'core');
+#import('../../dartlib/lib/html.dart');
 #import('vote.dart');
 #import('map.dart');
 
-#source('html/html_view.dart');
 #source('html/candidate_manager_view.dart');
 #source('html/plurality_view.dart');
 #source('html/distance_view.dart');
