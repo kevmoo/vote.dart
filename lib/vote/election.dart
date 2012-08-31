c5db4fc1f5463bf0a8e805b0603af3558113da07
Kevin Moore <kevin@thinkpixellab.com>
2012-08-30 22:31:44 -0400

    Election: fixed abstract property getters

--- lib/vote/election.dart
+++ lib/vote/election.dart
@@ -1,7 +1,7 @@
 class Election<TVoter extends Player, TCandidate extends Player> {
-  abstract Collection<TCandidate> get candidates();
-  abstract Iterable<Ballot<TVoter, TCandidate>> get ballots();
-  abstract ReadOnlyCollection<ElectionPlace<TCandidate>> get places();
+  abstract Collection<TCandidate> get candidates;
+  abstract Iterable<Ballot<TVoter, TCandidate>> get ballots;
+  abstract ReadOnlyCollection<ElectionPlace<TCandidate>> get places;
 
   TCandidate get singleWinner {
     if(places.length > 0 && places[0].length == 1) {
