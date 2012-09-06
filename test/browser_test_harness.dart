4f3b61df3483ddace2fa7a8824f05550bdc32450
Kevin Moore <kevin@thinkpixellab.com>
2012-09-06 14:41:46 -0400

    run all tests in the browser harness

--- test/browser_test_harness.dart
+++ test/browser_test_harness.dart
@@ -1,9 +1,13 @@
 #import('package:unittest/html_enhanced_config.dart');
 
 #import('vote/_vote_runner.dart');
+#import('map/_map_runner.dart');
+#import('calc/_calc_runner.dart');
 
 main() {
   useHtmlEnhancedConfiguration();
 
   runVoteTests();
+  runMapTests();
+  runCalcTests();
 }
