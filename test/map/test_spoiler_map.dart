fbca3ce71b2d6e30829e1095d54936ada8b02f9d
Kevin Moore <kevin@thinkpixellab.com>
2012-09-23 14:44:46 -0400

    using instance variables in TestSpoilerMap

--- test/map/test_spoiler_map.dart
+++ test/map/test_spoiler_map.dart
@@ -1,46 +1,49 @@
 class TestSpoilerMap {
-  static void run() {
+  final MapPlayer canCenter, canLeft, canSpoiler;
+  DistanceElection mapElection;
+
+  TestSpoilerMap() :
+    this.canCenter = new MapPlayer(const Coordinate(5, 0)),
+    this.canLeft = new MapPlayer(const Coordinate(4, 0)),
+    this.canSpoiler = new MapPlayer(const Coordinate(6.9, 0)) {
     // 11 voters from 0,0 to 10,0
     var voters = new List<MapPlayer>();
     for(var i = 0; i <= 10; i++) {
       voters.add(new MapPlayer(new Coordinate(i,0)));
     }
 
-    // center at 5
-    var canCenter = new MapPlayer(const Coordinate(5, 0));
+    mapElection = new DistanceElection(voters, [canCenter, canLeft, canSpoiler]);
+  }
 
-    // left at 4
-    var canLeft = new MapPlayer(const Coordinate(4, 0));
-
-    // spoiler at 7
-    var canSpoiler = new MapPlayer(const Coordinate(7, 0));
-
-    final mapElection = new DistanceElection(voters, [canCenter, canLeft, canSpoiler]);
-    final ballots = mapElection.ballots;
+  static void run() {
 
     group('spoiler', () {
       test('PluralityElection', (){
-        var election = new PluralityElection(ballots);
-        expect(election.singleWinner, equals(canLeft));
+        var test = new TestSpoilerMap();
+
+        var election = new PluralityElection(test.mapElection.ballots);
+        expect(election.singleWinner, equals(test.canLeft));
         expect(election.places.length, equals(3));
         expect(election.places[0].length, equals(1));
-        expect(election.places[0][0], equals(canLeft));
+        expect(election.places[0][0], equals(test.canLeft));
         expect(election.places[1].length, equals(1));
-        expect(election.places[1][0], equals(canSpoiler));
+        expect(election.places[1][0], equals(test.canSpoiler));
         expect(election.places[2].length, equals(1));
-        expect(election.places[2][0], equals(canCenter));
+        expect(election.places[2][0], equals(test.canCenter));
       });
 
       test('CondorcetElection', (){
-        var election = new CondorcetElection(ballots);
-        expect(election.singleWinner, equals(canCenter));
+        var test = new TestSpoilerMap();
+
+        var election = new CondorcetElection(test.mapElection.ballots);
+        expect(election.singleWinner, equals(test.canCenter));
         expect(election.places.length, equals(3));
         expect(election.places[0].length, equals(1));
-        expect(election.places[0][0], equals(canCenter));
+        expect(election.places[0][0], equals(test.canCenter));
         expect(election.places[1].length, equals(1));
-        expect(election.places[1][0], equals(canLeft));
+        expect(election.places[1][0], equals(test.canLeft));
         expect(election.places[2].length, equals(1));
-        expect(election.places[2][0], equals(canSpoiler));
+        expect(election.places[2][0], equals(test.canSpoiler));
       });
     });
   }
