d2d2d9beea6608c235181a4debdd1253b57efbd9
Kevin Moore <kevin@thinkpixellab.com>
2012-11-06 10:52:57 -0500

    new js hack for incompatible serializable object

--- web/vote_demo.dart.js
+++ web/vote_demo.dart.js
@@ -3700,7 +3700,8 @@ $$._MessageTraverser = {"":"Object;",
   return this.visitObject$1(x);
 },
  visitObject$1: function(x) {
-  throw $.$$throw("Message serialization: Illegal value " + $.S(x) + " passed");
+  // HACK. Just sending the object over the wire.
+  return x;
 }
 };
 
