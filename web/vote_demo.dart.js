eb3b2231852139a7b52810c7718556ead5c72df9
Kevin Moore <kevin@thinkpixellab.com>
2013-01-09 11:13:57 -0500

    js hack to get around serialization issues

--- web/vote_demo.dart.js
+++ web/vote_demo.dart.js
@@ -2456,7 +2456,9 @@ $$._MessageTraverser = {"":"Object;",
   return this.visitObject$1(x);
 },
  visitObject$1: function(x) {
-  throw $.$$throw("Message serialization: Illegal value " + $.S(x) + " passed");
+  // throw $.$$throw("Message serialization: Illegal value " + $.S(x) + " passed");
+  // HACK. Just sending the object over the wire.
+  return x;
 }
 };
 
