266e6795c4aa332da50b283366e39d9e6dd6d47d
Kevin Moore <github@j832.com>
2014-02-28 15:12:48 -0800

    using 'pub build'

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -11,17 +11,11 @@ void main(List<String> args) {
 
   addTask('analyze_libs', createAnalyzerTask(_getLibs(['lib', 'web'])));
 
-  //
-  // Dart2js
-  //
-  final paths = const ['web/vote_demo.dart', 'test/browser_test_harness.dart'];
-
-  addTask('dart2js', createDartCompilerTask(paths, minify: true));
   runHop(args);
 }
 
 Future<List<String>> _getLibs(Iterable<String> parentDirs) {
-  final files = new List<String>();
+  var files = new List<String>();
 
   return Future.forEach(parentDirs, (String d) {
     return new Directory(d).list()
@@ -36,10 +30,6 @@ Future<List<String>> _getLibs(Iterable<String> parentDirs) {
     .then((_) => files);
 }
 
-Future<bool> _ghPages(TaskContext ctx) {
-  final sourceDir = 'web';
-  final targetBranch = 'gh-pages';
-  final sourceBranch = 'master';
+Future<bool> _ghPages(TaskContext ctx) =>
+    branchForDir(ctx, 'master', 'build/web', 'gh-pages');
 
-  return branchForDir(ctx, sourceBranch, sourceDir, targetBranch);
-}
