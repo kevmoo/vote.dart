737698056e41157140818ecd058b220d25a875c9
Kevin Moore <kevmoo@google.com>
2019-05-30 12:52:30 -0700

    fix asserts

--- vote/lib/src/election_place.dart
+++ vote/lib/src/election_place.dart
@@ -9,7 +9,7 @@ class ElectionPlace<TCandidate extends Comparable>
 
   ElectionPlace(this.place, Iterable<TCandidate> candidates)
       : assert(place > 0),
-        assert(length > 0),
+        assert(candidates.isNotEmpty),
         super(candidates.toList()..sort());
 
   @override
