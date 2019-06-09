ff5dc871e51f5083dc287cf84d666f45e44197df
Kevin Moore <kevmoo@google.com>
2019-06-09 15:13:50 -0700

    DistanceElectionResultWidget: show place, name, distance

--- knarly/lib/src/distance_election_result_widget.dart
+++ knarly/lib/src/distance_election_result_widget.dart
@@ -7,17 +7,41 @@ class DistanceElectionResultWidget extends StatelessWidget {
   const DistanceElectionResultWidget();
 
   @override
-  Widget build(BuildContext context) => Consumer<VoteTown>(
-        builder: (context, voteTown, _) => ListView.builder(
-              padding: const EdgeInsets.all(8.0),
-              itemCount: voteTown.places.length,
-              itemBuilder: (ctx, index) {
-                final entry = voteTown.places[index];
-                final entryString = entry.map((s) => s.id).join(', ');
-                return Container(
-                  child: Text('$entryString - ${entry.averageDistance}'),
-                );
-              },
-            ),
+  Widget build(BuildContext context) => Container(
+        alignment: Alignment.center,
+        child: Consumer<VoteTown>(
+          builder: (context, voteTown, _) => ListView.builder(
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(8.0),
+                itemCount: voteTown.places.length,
+                itemBuilder: (ctx, index) {
+                  final entry = voteTown.places[index];
+                  final entryString = entry.map((s) => s.id).join(', ');
+                  return Row(children: [
+                    Expanded(
+                      child: Text(
+                        entry.place.toString(),
+                        textAlign: TextAlign.right,
+                      ),
+                      flex: 1,
+                    ),
+                    Expanded(
+                      child: Text(
+                        entryString,
+                        textAlign: TextAlign.right,
+                      ),
+                      flex: 1,
+                    ),
+                    Expanded(
+                      child: Text(
+                        entry.averageDistance.toString(),
+                        textAlign: TextAlign.right,
+                      ),
+                      flex: 1,
+                    ),
+                  ]);
+                },
+              ),
+        ),
       );
 }
