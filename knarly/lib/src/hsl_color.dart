e6bdba41c118e1dfd5be663c531ce8f459d14f80
Kevin Moore <kevmoo@google.com>
2019-07-10 16:52:13 -0700

    HslColor cleanup

--- knarly/lib/src/hsl_color.dart
+++ knarly/lib/src/hsl_color.dart
@@ -4,35 +4,29 @@ import 'package:flutter_web_ui/ui.dart';
 // http://code.google.com/p/closure-library/source/browse/trunk/closure/goog/color/color.js
 
 class HslColor {
-  final num h, s, l;
+  final double h, s, l;
 
   const HslColor._internal(this.h, this.s, this.l);
 
-  factory HslColor(num h, num s, num l) => HslColor._internal(h % 360, s, l);
+  factory HslColor(double h, double s, double l) =>
+      HslColor._internal(h % 360, s, l);
 
   Color toColor() {
     final normH = h / 360; // normalize h to fall in [0, 1]
 
-    num r, g, b;
+    double r, g, b;
 
     if (s == 0) {
       r = g = b = l * 255;
     } else {
-      num temp1 = 0;
-      num temp2 = 0;
-      if (l < 0.5) {
-        temp2 = l * (1 + s);
-      } else {
-        temp2 = l + s - (s * l);
-      }
-      temp1 = 2 * l - temp2;
+      final temp2 = (l < 0.5) ? l * (1 + s) : l + s - (s * l);
+      final temp1 = 2 * l - temp2;
       r = 255 * _hueToRgb(temp1, temp2, normH + (1 / 3));
       g = 255 * _hueToRgb(temp1, temp2, normH);
       b = 255 * _hueToRgb(temp1, temp2, normH - (1 / 3));
     }
 
-    return Color.fromRGBO(
-        r.round().toInt(), g.round().toInt(), b.round().toInt(), 1);
+    return Color.fromRGBO(r.round(), g.round(), b.round(), 1);
   }
 
   @override
@@ -45,7 +39,7 @@ class HslColor {
   @override
   String toString() => '{HslColor: $h, $s, $l}';
 
-  static num _hueToRgb(num v1, num v2, num vH) {
+  static double _hueToRgb(double v1, double v2, num vH) {
     vH %= 1.0;
 
     if ((6 * vH) < 1) {
