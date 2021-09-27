8fe01f6575cd50e4fe14b0d710f94ff1f25284ad
Kevin Moore <kevmoo@google.com>
2021-09-26 17:54:11 -0700

    demo: descriptive headers!

--- vote_demo/lib/src/vote_simulation.dart
+++ vote_demo/lib/src/vote_simulation.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:vote/vote.dart';
 import 'package:vote_widgets/vote_widgets.dart';
 
@@ -9,6 +11,7 @@ import 'model/vote_town.dart';
 import 'model/vote_town_distance_place.dart';
 import 'view_model/vote_town_editor.dart';
 import 'widget/distance_election_result_widget.dart';
+import 'widget/header_widget.dart';
 import 'widget/vote_town_widget.dart';
 
 const _sourceUrl = 'github.com/kevmoo/vote.dart';
@@ -76,32 +79,77 @@ class VoteSimulation extends StatelessWidget {
       );
 
   List<Widget> _columnOneChildren(VoteTownEditor kvm) => <Widget>[
-        ListenableProvider<VoteTownEditor>.value(
-          value: kvm,
-          child: const VoteTownWidget(),
+        HeaderWidget(
+          header: 'Vote Town',
+          extraHelp:
+              'A simple town of 100 people trying to figure out where their '
+              'post office should go.',
+          child: ListenableProvider<VoteTownEditor>.value(
+            value: kvm,
+            child: const VoteTownWidget(),
+          ),
         ),
-        Provider<List<VoteTownDistancePlace>>.value(
-          value: (kvm.value).distancePlaces,
-          child: const DistanceElectionResultWidget(),
+        HeaderWidget(
+          header: 'Place by average distance',
+          extraHelp:
+              'The candidate locations ranked by their distance to the "ideal" '
+              'location in the center of town.',
+          child: Provider<List<VoteTownDistancePlace>>.value(
+            value: (kvm.value).distancePlaces,
+            child: const DistanceElectionResultWidget(),
+          ),
         ),
       ];
 
   List<Widget> _columnTwoChildren(VoteTownEditor kvm) => [
-        _HeaderWidget(
+        HeaderWidget(
           header: 'Plurality',
+          extraHelp: TextSpan(children: [
+            const TextSpan(
+              text: 'The result of a typical "pick your favorite" election - '
+                  'as if every voter cast a ballot for just their favorite '
+                  '(closest) candidate. See ',
+            ),
+            _linkSpan('https://wikipedia.org/wiki/Plurality_voting'),
+          ]),
           child: Provider<PluralityElection<Candidate>>.value(
             value: kvm.value.pluralityElection,
             child: const PluralityElectionResultWidget(),
           ),
         ),
-        _HeaderWidget(
-          header: 'Condorcet',
+        HeaderWidget(
+          header: 'Ranked Pairs',
+          extraHelp: TextSpan(children: [
+            const TextSpan(text: 'A '),
+            _linkSpan(
+              'https://en.wikipedia.org/wiki/Ranked_voting',
+              text: 'ranked voting method',
+            ),
+            const TextSpan(
+              text: ' which calculates the winner by evaluating every pair of '
+                  'candidates. See ',
+            ),
+            _linkSpan('https://wikipedia.org/wiki/Ranked_pairs'),
+          ]),
           child: CondorcetElectionResultWidget<Candidate>(
             election: kvm.value.condorcetElection,
           ),
         ),
-        _HeaderWidget(
-          header: 'Ranked Choice',
+        HeaderWidget(
+          header: 'Instant-runoff voting',
+          extraHelp: TextSpan(children: [
+            const TextSpan(text: 'A '),
+            _linkSpan(
+              'https://en.wikipedia.org/wiki/Ranked_voting',
+              text: 'ranked voting method',
+            ),
+            const TextSpan(
+              text: ' which calculates the winner by repeatedly calculating '
+                  'run-offs where the candidate with the fewest #1 rankings is '
+                  'eliminated. See ',
+            ),
+            _linkSpan('https://wikipedia.org/wiki/Instant-runoff_voting'),
+          ]),
           child: Provider<IrvElection<Candidate>>.value(
             value: kvm.value.irvElection,
             child: const RankedChoiceElectionResultWidget(),
@@ -110,29 +158,25 @@ class VoteSimulation extends StatelessWidget {
       ];
 }
 
-const _crossAxisWidth = 500.0;
-
-class _HeaderWidget extends StatelessWidget {
-  final String header;
-  final Widget child;
-  const _HeaderWidget({
-    Key? key,
-    required this.header,
-    required this.child,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.all(5),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text(
-              header,
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            child,
-          ],
-        ),
-      );
+String _trimUrl(String url) {
+  const httpsStart = 'https://';
+  if (url.startsWith(httpsStart)) {
+    return url.substring(httpsStart.length);
+  }
+  return url;
 }
+
+TextSpan _linkSpan(String url, {String? text}) => TextSpan(
+      text: text ?? _trimUrl(url),
+      recognizer: TapGestureRecognizer()
+        ..onTap = () {
+          launch(url);
+        },
+      style: const TextStyle(
+        decoration: TextDecoration.underline,
+        color: Colors.lightBlue,
+      ),
+      mouseCursor: MaterialStateMouseCursor.clickable,
+    );
+
+const _crossAxisWidth = 500.0;
---
+++ vote_demo/lib/src/widget/header_widget.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class HeaderWidget extends StatefulWidget {
+  final String header;
+  final Widget child;
+  final Object? extraHelp;
+
+  const HeaderWidget({
+    Key? key,
+    required this.header,
+    required this.child,
+    this.extraHelp,
+  }) : super(key: key);
+
+  @override
+  _HeaderWidgetState createState() => _HeaderWidgetState();
+}
+
+class _HeaderWidgetState extends State<HeaderWidget> {
+  ScaffoldFeatureController? _snackbarController;
+
+  @override
+  void dispose() {
+    _closeSnackbarIfOpen();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Row(children: [
+              Flexible(child: Container()),
+              Expanded(
+                flex: 6,
+                child: Text(
+                  widget.header,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+              ),
+              Flexible(
+                child: Container(
+                  alignment: Alignment.centerRight,
+                  child: widget.extraHelp == null
+                      ? null
+                      : IconButton(
+                          icon: const Icon(Icons.info_outline),
+                          onPressed: _snackHandler(context),
+                        ),
+                ),
+              )
+            ]),
+            widget.child,
+          ],
+        ),
+      );
+
+  Widget _extraHelpContent() {
+    final extraHelp = widget.extraHelp!;
+    if (extraHelp is String) {
+      return SelectableText(extraHelp);
+    }
+
+    if (extraHelp is TextSpan) {
+      return SelectableText.rich(extraHelp);
+    }
+
+    return Text('??? $extraHelp ???');
+  }
+
+  void Function() _snackHandler(BuildContext context) => () {
+        final messenger = ScaffoldMessenger.of(context);
+
+        if (_closeSnackbarIfOpen()) {
+          return;
+        }
+
+        final snackBar = SnackBar(
+          content: _extraHelpContent(),
+          action: SnackBarAction(
+            label: 'Close',
+            onPressed: _closeSnackbarIfOpen,
+          ),
+        );
+
+        messenger.clearSnackBars();
+
+        _snackbarController = messenger.showSnackBar(snackBar);
+        _snackbarController!.closed.then((value) {
+          _snackbarController = null;
+        });
+      };
+
+  bool _closeSnackbarIfOpen() {
+    final controller = _snackbarController;
+    if (controller != null) {
+      // still open!
+      controller.close();
+      _snackbarController = null;
+      return true;
+    }
+    return false;
+  }
+}
