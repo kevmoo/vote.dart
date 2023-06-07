f96bd3246a6cfaaf9924f1b05c97f2d2eb6afe4f
Kevin Moore <kevmoo@google.com>
2023-06-06 17:20:38 -0700

    Remove platform silly

--- vote_demo/lib/main.dart
+++ vote_demo/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/widgets.dart';
 
-import 'src/my_platform.dart' if (dart.library.html) 'src/my_platform_web.dart';
 import 'src/vote_simulation.dart';
 
 void main() {
-  configurePlatform();
   runApp(VoteSimulation());
 }
