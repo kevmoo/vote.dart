1dc174c85a3af97053d9c7df426b9df0ec63eb70
Kevin Moore <kevmoo@google.com>
2025-02-13 12:07:05 -0800

    Built web
    Branch: master
    Commit: f6064191852cf2c766bd937e6ebcefe4a2aa2d9e
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"78e5d7995cdb16e0b8a3eb12bb62ff325e8c601d","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"4ff2deafd4b9d9b8bf030db8b2163b49ecdccf10","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1585311392"
+    serviceWorkerVersion: "540817880"
   }
 });
