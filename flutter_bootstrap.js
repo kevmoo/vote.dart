6fd3463be1f13f0748d49e6296e5942afef06320
Kevin Moore <kevmoo@google.com>
2025-04-24 14:10:17 -0700

    Built web
    Branch: master
    Commit: 7d38eb200244f98245f97e43bd56f4fa4b1cda24
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -34,11 +34,11 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"127faefdf2c87260e36e3f158bc161b87161cfe9","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"ce51065f81ae69e649cb96500ee7d1566fee7c98","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "650890149"
+    serviceWorkerVersion: "2870667368"
   }
 });
